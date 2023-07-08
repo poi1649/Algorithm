4b3d01c6ba320250688544ac08ec6ac7b2901436
Bojun Kim <qhwns1649@naver.com>
2023-07-08 20:24:19 +0900

    [Silver II] Title: 최대 힙, Time: 348 ms, Memory: 26196 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> priorityQueueLowest = new PriorityQueue<>(Collections.reverseOrder());
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            final int target = Integer.parseInt(br.readLine());
+            if (target == 0) {
+                if (priorityQueueLowest.isEmpty()){
+                    sb.append(0).append(System.lineSeparator());
+                    continue;
+                }
+                sb.append(priorityQueueLowest.poll()).append(System.lineSeparator());
+                continue;
+            }
+            priorityQueueLowest.add(target);
+        }
+        System.out.print(sb);
+    }
+}
+
+
