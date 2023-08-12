ad2a7291b5266daa50a0150ceb2ebb920a92a5ed
Bojun Kim <qhwns1649@naver.com>
2023-08-12 13:06:22 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 224 ms, Memory: 19516 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,163 @@
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class Main {
+
+    static StringBuilder sb = new StringBuilder();
+    static int n;
+    static long count;
+
+
+    public static int recursion(String s, int l, int r){
+        count++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new java.io.InputStreamReader(System.in));
+        n = parseInt(reader.readLine());;
+        for (int i = 0; i < n; i++) {
+            final String s = reader.readLine();
+            count = 0;
+            sb.append(isPalindrome(s)).append(" ").append(count).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
+
+
+class Reader {
+
+    final private int BUFFER_SIZE = 1 << 16;
+    private DataInputStream din;
+    private byte[] buffer;
+    private int bufferPointer, bytesRead;
+
+    public Reader() {
+        din = new DataInputStream(System.in);
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public Reader(String file_name) throws IOException {
+        din = new DataInputStream(
+                new FileInputStream(file_name));
+        buffer = new byte[BUFFER_SIZE];
+        bufferPointer = bytesRead = 0;
+    }
+
+    public String readLine() throws IOException {
+        byte[] buf = new byte[64]; // line length
+        int cnt = 0, c;
+        while ((c = read()) != -1) {
+            if (c == '\n') {
+                if (cnt != 0) {
+                    break;
+                } else {
+                    continue;
+                }
+            }
+            buf[cnt++] = (byte) c;
+        }
+        return new String(buf, 0, cnt);
+    }
+
+    public int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg) {
+            c = read();
+        }
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (neg) {
+            return -ret;
+        }
+        return ret;
+    }
+
+    public long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg) {
+            c = read();
+        }
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) {
+            return -ret;
+        }
+        return ret;
+    }
+
+    public double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') {
+            c = read();
+        }
+        boolean neg = (c == '-');
+        if (neg) {
+            c = read();
+        }
+
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+
+        if (c == '.') {
+            while ((c = read()) >= '0' && c <= '9') {
+                ret += (c - '0') / (div *= 10);
+            }
+        }
+
+        if (neg) {
+            return -ret;
+        }
+        return ret;
+    }
+
+    private void fillBuffer() throws IOException {
+        bytesRead = din.read(buffer, bufferPointer = 0,
+                BUFFER_SIZE);
+        if (bytesRead == -1) {
+            buffer[0] = -1;
+        }
+    }
+
+    private byte read() throws IOException {
+        if (bufferPointer == bytesRead) {
+            fillBuffer();
+        }
+        return buffer[bufferPointer++];
+    }
+
+    public void close() throws IOException {
+        if (din == null) {
+            return;
+        }
+        din.close();
+    }
+}
+
