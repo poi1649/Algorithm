952bd6fd580f1b45fc9b45942ade65f57e2e941f
Bojun Kim <qhwns1649@naver.com>
2023-08-31 21:27:00 +0900

    [Platinum V] Title: 최솟값 찾기, Time: 260 ms, Memory: 98208 KB -BaekjoonHub

--- 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
+++ 백준/Platinum/11003. 최솟값 찾기/최솟값 찾기.cc
@@ -1,43 +1,139 @@
+//구현 2. mmap/write 이용
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2")
 #include <bits/stdc++.h>
-
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
 using namespace std;
 
-int n, l;
-vector<int> tree;
+/////////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * Author : jinhan814
+ * Date : 2021-05-06
+ * Source : https://blog.naver.com/jinhan814/222266396476
+ * Description : FastIO implementation for cin, cout. (mmap ver.)
+ */
+constexpr int SZ = 1 << 20;
 
-void update(int index, int value) {
-    index += tree.size() / 2;
-    tree[index] = value;
-    while (index > 1) {
-        index /= 2;
-        tree[index] = min(tree[index * 2], tree[index * 2 + 1]);
+class INPUT {
+private:
+    char* p;
+    bool __END_FLAG__, __GETLINE_FLAG__;
+public:
+    explicit operator bool() { return !__END_FLAG__; }
+    INPUT() {
+        struct stat st; fstat(0, &st);
+        p = (char*)mmap(0, st.st_size, PROT_READ, MAP_SHARED, 0, 0);
     }
-}
-
-int findMin() {
-    return tree[1];
-}
-
-void buildMin() {
-    for (int i = tree.size() / 2 - 1; i > 0; i--) {
-        tree[i] = min(tree[i * 2], tree[i * 2 + 1]);
+    bool IsBlank(char c) { return c == ' ' || c == '\n'; }
+    bool IsEnd(char c) { return c == '\0'; }
+    char _ReadChar() { return *p++; }
+    char ReadChar() {
+        char ret = _ReadChar();
+        for (; IsBlank(ret); ret = _ReadChar());
+        return ret;
     }
-}
+    template<typename T> T ReadInt() {
+        T ret = 0; char cur = _ReadChar(); bool flag = 0;
+        for (; IsBlank(cur); cur = _ReadChar());
+        if (cur == '-') flag = 1, cur = _ReadChar();
+        for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret = 10 * ret + (cur & 15);
+        if (IsEnd(cur)) __END_FLAG__ = 1;
+        return flag ? -ret : ret;
+    }
+    string ReadString() {
+        string ret; char cur = _ReadChar();
+        for (; IsBlank(cur); cur = _ReadChar());
+        for (; !IsBlank(cur) && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+        if (IsEnd(cur)) __END_FLAG__ = 1;
+        return ret;
+    }
+    double ReadDouble() {
+        string ret = ReadString();
+        return stod(ret);
+    }
+    string getline() {
+        string ret; char cur = _ReadChar();
+        for (; cur != '\n' && !IsEnd(cur); cur = _ReadChar()) ret.push_back(cur);
+        if (__GETLINE_FLAG__) __END_FLAG__ = 1;
+        if (IsEnd(cur)) __GETLINE_FLAG__ = 1;
+        return ret;
+    }
+    friend INPUT& getline(INPUT& in, string& s) { s = in.getline(); return in; }
+} _in;
+
+class OUTPUT {
+private:
+    char write_buf[SZ];
+    int write_idx;
+public:
+    ~OUTPUT() { Flush(); }
+    explicit operator bool() { return 1; }
+    void Flush() {
+        write(1, write_buf, write_idx);
+        write_idx = 0;
+    }
+    void WriteChar(char c) {
+        if (write_idx == SZ) Flush();
+        write_buf[write_idx++] = c;
+    }
+    template<typename T> int GetSize(T n) {
+        int ret = 1;
+        for (n = n >= 0 ? n : -n; n >= 10; n /= 10) ret++;
+        return ret;
+    }
+    template<typename T> void WriteInt(T n) {
+        int sz = GetSize(n);
+        if (write_idx + sz >= SZ) Flush();
+        if (n < 0) write_buf[write_idx++] = '-', n = -n;
+        for (int i = sz; i --> 0; n /= 10) write_buf[write_idx + i] = n % 10 | 48;
+        write_idx += sz;
+    }
+    void WriteString(string s) { for (auto& c : s) WriteChar(c); }
+    void WriteDouble(double d) { WriteString(to_string(d)); }
+} _out;
+
+/* operators */
+INPUT& operator>> (INPUT& in, char& i) { i = in.ReadChar(); return in; }
+INPUT& operator>> (INPUT& in, string& i) { i = in.ReadString(); return in; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+INPUT& operator>> (INPUT& in, T& i) {
+    if constexpr (is_floating_point_v<T>) i = in.ReadDouble();
+    else if constexpr (is_integral_v<T>) i = in.ReadInt<T>(); return in; }
+
+OUTPUT& operator<< (OUTPUT& out, char i) { out.WriteChar(i); return out; }
+OUTPUT& operator<< (OUTPUT& out, string i) { out.WriteString(i); return out; }
+template<typename T, typename std::enable_if_t<is_arithmetic_v<T>>* = nullptr>
+OUTPUT& operator<< (OUTPUT& out, T i) {
+    if constexpr (is_floating_point_v<T>) out.WriteDouble(i);
+    else if constexpr (is_integral_v<T>) out.WriteInt<T>(i); return out; }
+
+/* macros */
+#define fastio 1
+#define cin _in
+#define cout _out
+#define istream INPUT
+#define ostream OUTPUT
+/////////////////////////////////////////////////////////////////////////////////////////////
+
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    fastio;
+    int n, l;
     cin >> n >> l;
-    int k = 2;
-    while (k < 2 * l) {
-        k *= 2;
-    }
-    tree = vector<int>(k, INT_MAX);
+    deque<pair<int, int>> deque;
     for (int i = 0; i < n; i++) {
-        int index = (i % l);
         int x;
         cin >> x;
-        update(index, x);
-        cout << findMin() << " ";
+        while (!deque.empty() && deque.back().first >= x) {
+            deque.pop_back();
+        }
+        deque.push_back({x, i});
+        if (deque.front().second == i - l) {
+            deque.pop_front();
+        }
+        cout << deque.front().first << ' ';
     }
+    return 0;
 }
