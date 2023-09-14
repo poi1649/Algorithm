bbd78378e2584d6279c927a2eb7cab2d21e01720
Bojun Kim <qhwns1649@naver.com>
2023-09-14 17:37:04 +0900

    [Platinum II] Title: 학교 가지마!, Time: 4 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Platinum/1420. 학교 가지마！/학교 가지마！.cc
+++ 백준/Platinum/1420. 학교 가지마！/학교 가지마！.cc
@@ -2,102 +2,147 @@
 
 using namespace std;
 
-typedef pair<int, int> p;
-const int inf = 1e9;
-
-char arr[101][101];
-vector<int> g[23000];
-int par[23000];
-map<p, int> c, f;
+int lv[23000];
+int work[23000];
+int n;
 int s = -1, t = -1;
-int si, sj, ti, tj;
+bool inputs[101][101];
 
-int n, m;
+struct Edge {
+    int from, to, cap, flw;
+    Edge *rev;
 
-void addEdge(int s, int e, int x) {
-    g[s].push_back(e);
-    c[{s, e}] = x;
-    g[e].push_back(s);
-    c[{e, s}] = 0;
+    Edge(int u, int v, int c) : from(u), to(v), cap(c), flw(0) {}
+
+    int remain() { return cap - flw; }
+
+    void add_flow(int amount) {
+        flw += amount;
+        rev->flw -= amount;
+    }
+};
+
+vector<Edge *> g2[23000];
+
+void add_line(int from, int to, int capacity) {
+    Edge *e = new Edge(from, to, capacity);
+    Edge *revE = new Edge(to, from, 0);
+    e->rev = revE, revE->rev = e;
+    g2[from].push_back(e), g2[to].push_back(revE);
 }
 
-int run() {
-    int ret = 0;
-    while (1) {
-        memset(par, -1, sizeof par);
-        queue<int> q;
-        q.push(s);
-        while (q.size()) {
-            int now = q.front();
-            q.pop();
-            for (auto nxt: g[now]) {
-                if (par[nxt] == -1 && c[{now, nxt}] - f[{now, nxt}] > 0) {
-                    q.push(nxt);
-                    par[nxt] = now;
-                }
+bool bfs() {
+    memset(lv, -1, sizeof lv);
+    queue<int> q;
+    q.push(s);
+    lv[s] = 0;
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+        for (auto nxtE: g2[now]) {
+            int nxt = nxtE->to;
+            if (lv[nxt] == -1 && nxtE->remain() > 0) {
+                lv[nxt] = lv[now] + 1;
+                q.push(nxt);
             }
         }
-        if (par[t] == -1) break;
-        for (int i = t; i != s; i = par[i]) {
-            int a = par[i], b = i;
-            f[{a, b}]++;
-            f[{b, a}]--;
-        }
-        ret++;
     }
-    return ret;
+    return lv[t] != -1;
 }
+
+int dfs(int now, int tot) {
+    if (now == t) return tot;
+    for (int &i = work[now]; i < g2[now].size(); i++) {
+        Edge *&nextE = g2[now][i];
+        int nxt = nextE->to;
+        if (lv[nxt] == lv[now] + 1 && nextE->remain() > 0) {
+            int fl = dfs(nxt, min(tot, nextE->remain()));
+            if (fl > 0) {
+                nextE->add_flow(fl);
+                return fl;
+            }
+        }
+    }
+    return 0;
+}
+
+
+int m;
+string a;
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cin >> n >> m;
 
-    int pv = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            cin >> arr[i][j];
-            if (arr[i][j] == 'K') {
-                s = pv + 1;
-                si = i, sj = j;
+    memset(inputs, 0, sizeof inputs);
+    int sX, sY, eX, eY;
+
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= m; j++) add_line(((i - 1) * m + j) * 2, ((i - 1) * m + j) * 2 + 1, 1);
+
+
+    for (int i = 1; i <= n; ++i) {
+        cin >> a;
+        for (int j = 0; j < m; ++j) {
+            if (a[j] == '#') {
+                continue;
             }
-            if (arr[i][j] == 'H') {
-                t = pv;
-                ti = i, tj = j;
+            if (a[j] == '.') {
+                inputs[i][j + 1] = true;
             }
-            pv += 2;
+            if (a[j] == 'K') {
+                inputs[i][j + 1] = true;
+                s = (((i - 1) * m) + j + 1) * 2 + 1;
+                sX = i, sY = j;
+            }
+            if (a[j] == 'H') {
+                inputs[i][j + 1] = true;
+                t = (((i - 1) * m) + j + 1) * 2;
+                eX = i, eY = j;
+            }
         }
     }
 
-    if (n == 1 && m == 1) {
-        cout << -1;
+    if (sX == eX - 1 && sY == eY) {
+        cout << -1 << "\n";
         return 0;
     }
-    if (abs(si - ti) + abs(sj - tj) == 1 || s == -1 || t == -1) {
-        cout << -1;
+    if (sX == eX && sY == eY - 1) {
+        cout << -1 << "\n";
+        return 0;
+    }
+    if (sX == eX && sY == eY + 1) {
+        cout << -1 << "\n";
+        return 0;
+    }
+    if (sX == eX + 1 && sY == eY) {
+        cout << -1 << "\n";
         return 0;
     }
 
-    for (int i = 0; i < n * m; i++) {
-        addEdge(2 * i, 2 * i + 1, 1);
-    }
-
-    pv = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (i + 1 < n && arr[i][j] != '#' && arr[i + 1][j] != '#') {
-                int nxt = pv + 2 * m;
-                addEdge(pv + 1, nxt, inf);
-                addEdge(nxt + 1, pv, inf);
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (!inputs[i][j]) continue;
+            if (i < n && inputs[i + 1][j]) {
+                add_line(((i - 1) * m + j) * 2 + 1, (i * m + j) * 2, 1);
+                add_line(((i * m + j) * 2 + 1), ((i - 1) * m + j) * 2, 1);
             }
-            if (j + 1 < m && arr[i][j] != '#' && arr[i][j + 1] != '#') {
-                int nxt = pv + 2;
-                addEdge(pv + 1, nxt, inf);
-                addEdge(nxt + 1, pv, inf);
+            if (j < m && inputs[i][j + 1]) {
+                add_line(((i - 1) * m + j + 1) * 2 + 1, ((i - 1) * m + j) * 2, 1);
+                add_line(((i - 1) * m + j) * 2 + 1, ((i - 1) * m + j + 1) * 2, 1);
             }
-            pv += 2;
         }
     }
-
-    cout << run();
+    int ans = 0;
+    while (bfs()) {
+        memset(work, 0, sizeof work);
+        while (true) {
+            int fl = dfs(s, 1e9 + 7);
+            if (!fl) break;
+            ans += fl;
+        }
+    }
+    cout << ans << "\n";
+    return 0;
 }
