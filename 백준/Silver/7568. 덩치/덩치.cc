5ba147e32c9d32c8e0726c5c51cfb9b3a247a77a
Bojun Kim <qhwns1649@naver.com>
2023-09-28 20:37:10 +0900

    [Silver V] Title: 덩치, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.cc
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+
+//const int TREE_SIZE = 2097152;
+////const int TREE_SIZE = 16;
+//int n, m, k;
+//ll tree[TREE_SIZE];
+//ll lazy[TREE_SIZE];
+//
+//void init() {
+//    for (int i = TREE_SIZE / 2 - 1; i > 0; i--) {
+//        tree[i] = tree[i * 2] + tree[i * 2 + 1];
+//    }
+//}
+//
+//void propagate(int node, int l, int r) {
+//    if (lazy[node] != 0) {
+//        tree[node] += (r - l + 1) * lazy[node];
+//        if (l != r) {
+//            lazy[node * 2] += lazy[node];
+//            lazy[node * 2 + 1] += lazy[node];
+//        }
+//        lazy[node] = 0;
+//    }
+//}
+//
+//void add_w(int l, int r, ll w, int node = 1, int nl = 1, int nr = TREE_SIZE / 2) {
+//    propagate(node, nl, nr);
+//    if (r < nl || nr < l) {
+//        return;
+//    }
+//    if (l <= nl && nr <= r) {
+//        lazy[node] += w;
+//        propagate(node, nl, nr);
+//        return;
+//    }
+//    int mid = (nl + nr) / 2;
+//    add_w(l, r, w, node * 2, nl, mid);
+//    add_w(l, r, w, node * 2 + 1, mid + 1, nr);
+//    tree[node] = tree[node * 2] + tree[node * 2 + 1];
+//}
+//
+//ll get_w(int l, int r, int node = 1, int nl = 1, int nr = TREE_SIZE / 2) {
+//    propagate(node, nl, nr);
+//    if (r < nl || nr < l) {
+//        return 0;
+//    }
+//    if (l <= nl && nr <= r) {
+//        return tree[node];
+//    }
+//    int mid = (nl + nr) / 2;
+//    return get_w(l, r, node * 2, nl, mid) + get_w(l, r, node * 2 + 1, mid + 1, nr);
+//}
+//
+//void print_tree() {
+////    cout << "tree:\n";
+////    for (int i = 0; i < TREE_SIZE; ++i) {
+////        cout << i << " ";
+////    }
+////    cout << "\n";
+////    for (auto x : tree) {
+////        cout << x << " ";
+////    }
+////    cout << "\n";
+//}
+
+int n, x, y;
+
+void process() {
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> x >> y;
+        a[i] = {x, y};
+    }
+    for (auto &x1 : a) {
+        int count = 1;
+        for (auto &y1 : a) {
+            if (x1.first < y1.first && x1.second < y1.second) {
+                count++;
+            }
+        }
+        cout << count << " ";
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int o = 1;
+//    cin >> o;
+    while (o--) {
+        process();
+    }
+    return 0;
+}
