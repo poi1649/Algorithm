e0b41f820d81dc92c09d69281bb86816731217fd
Bojun Kim <qhwns1649@naver.com>
2023-11-06 14:32:23 +0900

    [Bronze I] Title: 접두사 배열, Time: 228 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Bronze/13322. 접두사 배열/접두사 배열.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    static int t;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        t = parseInt(br.readLine());
+        t = 1;
+        int cur = 1;
+        while (t-- > 0) {
+            process(br, cur++);
+        }
+        br.close();
+    }
+
+    public static void process(BufferedReader br, int t) throws IOException {
+        var s = br.readLine().toCharArray();
+        for (int i = 0; i < s.length; i++) {
+            sb.append(i).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
+
+
