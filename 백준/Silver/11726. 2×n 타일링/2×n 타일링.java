2d6a19f67910a4f25538bcb18a3009553557a311
Bojun Kim <qhwns1649@naver.com>
2023-11-20 15:14:00 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,36 @@
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int t;
+    static int m;
+    static int n;
+    static int[] dp = new int[1001];
+    static int MOD = 10007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        t = parseInt(br.readLine());
+        t = 1;
+        int cur = 1;
+        while (t-- > 0) {
+            process(br, cur++);
+        }
+        br.close();
+    }
+
+    public static void process(BufferedReader br, int cur) throws IOException {
+        n = parseInt(br.readLine());
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= MOD;
+        }
+        System.out.println(dp[n]);
+    }
+}
