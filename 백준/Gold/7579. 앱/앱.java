ad071521ad8f92a068f1fc4b655ead88cf280814
Bojun Kim <qhwns1649@naver.com>
2023-11-28 14:58:46 +0900

    [Gold III] Title: 앱, Time: 244 ms, Memory: 20696 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,44 +1,62 @@
+import static java.lang.Integer.parseInt;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    static int q, n, m;
+    static int[][] dp = new int[10001][101];
 
-        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final String[] s = br.readLine().split(" ");
-        final int n = Integer.parseInt(s[0]);
-        final int m = Integer.parseInt(s[1]);
-        int[] temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        final int[] memories = new int[n + 1];
-        System.arraycopy(temp, 0, memories, 1, n);
-        temp = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        final int[] costs = new int[n + 1];
-        System.arraycopy(temp, 0, costs, 1, n);
-        final int[][] dp = new int[n + 1][10001];
+    static class Node {
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= 10000; j++) {
-                if (j < costs[i]) {
-                    dp[i][j] = dp[i - 1][j];
-                    continue;
-                }
-                dp[i][j] = Math.max(
-                        dp[i - 1][j],
-                        dp[i - 1][j - costs[i]] + memories[i]
-                );
-            }
+        int m, c;
+
+        public Node(final int m, final int c) {
+            this.m = m;
+            this.c = c;
         }
+    }
 
-        for (int i = 1; i <= 10000; i++) {
-            if (dp[n][i] >= m) {
-                System.out.println(i);
-                return;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        q = 1;
+        int cur = 1;
+        while (q-- > 0) {
+            process(br, cur++);
+        }
+        br.close();
+    }
+
+    public static void process(BufferedReader br, int cur) throws IOException {
+        final var s = br.readLine().split(" ");
+        n = parseInt(s[0]);
+        m = parseInt(s[1]);
+        List<Node> nodes = new ArrayList<>();
+        final var ms = br.readLine().split(" ");
+        final var cs = br.readLine().split(" ");
+        for (int i = 1; i <= n; i++) {
+            nodes.add(new Node(parseInt(ms[i - 1]), parseInt(cs[i - 1])));
+        }
+        nodes.sort(Comparator.comparingInt(o -> o.c));
+        for (int i = 0; i <= 10000; i++) {
+            for (int j = 1; j <= nodes.size(); j++) {
+                final var node = nodes.get(j - 1);
+                if (i - node.c >= 0) {
+                    dp[i][j] = node.m;
+                    for (int k = 1; k < j; k++) {
+                        dp[i][j] = Math.max(dp[i][j], dp[i - node.c][k] + node.m);
+                    }
+                }
+                if (dp[i][j] >= m) {
+                    System.out.println(i);
+                    return;
+                }
             }
         }
     }
 }
-
-
